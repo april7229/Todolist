29d73d01cb045494b770be627e19ee47c7ad09fd
april <martinezapril56@yahoo.com>
2019-09-05 22:59:44 -0400

    making the icons work

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { FaChevronDown,
+    FaRegCalendar,
+    FaRegCalendarAlt,
+    FaInbox,
+} from 'react-icons/fa';
 
  
 
 
-export const Sidebar = () => <p>I am a side bar</p>
+export const Sidebar = () => (
+<div className = "sidebar" data-testid="sidebar">
+    <ul className="sidebar__generic">
+        <li>
+        <span>
+            <FaInbox />
+            </span>
+            <span>Inbox</span>
+        </li>
+            <span>
+                <FaRegCalendar />
+            </span>
+        <span>Today</span>
+
+        <li>Next 7 days </li>
+    </ul>
+    </div>
     
-        
+
+    
+);
     
