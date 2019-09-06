30f9f8d36b16c769ded03e9bf1b54ead60a0bf12
april <martinezapril56@yahoo.com>
2019-09-05 21:30:00 -0400

    april

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Header} from './components/Header';
-
+import {Content} from './components/layout/Content';
 
 
 export const App =() => (
   
     <div className="App">
       <Header />
+      <Content />
     </div>
 
 );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 //inline export
 import React from 'react';
+import {FaPizzaSlice} from 'react-icons/fa';
 
 export const Header = () => {
     const x = 1;
@@ -7,14 +8,18 @@ export const Header = () => {
         <header className = "header" data-testid="header">
 
             <nav>
+
             <div className="logo">
                 <img src = "/images/logo.png" alt="Todolist" />
 
             </div>
-            <div className="settings"></div>
+
+            <div className="settings">
             <ul>
-                
+            <li>++</li>
+            <li><FaPizzaSlice /></li>
             </ul>
+            </div>
             </nav>
         
         </header>
